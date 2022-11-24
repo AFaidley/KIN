ed5b9ef1ca20fa8477ef719d44c401b80612dc56
thaling <thaling@verizon.net>
2022-11-23 17:55:58 -0700

    connecting server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,51 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import Homepage from './pages/Homepage';
+import Addiction from './pages/Addiction';
+import Chronic from './pages/Chronic';
+import Grief from './pages/Grief';
+import Mental from './pages/Mental';
+import Physical from './pages/Physical';
+import PTSD from './pages/PTSD';
+import Userpage from './pages/Userpage';
+import AppNavbar from './components/Navbar';
+
+const Http = createHttpLink({
+  uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(Http),
+    cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
     <ApolloProvider client={client}>
     <Router>
       <>
-        <Navbar />
+        <AppNavbar />
         <Routes>
           <Route 
             path='/' 
             element={<Homepage />} 
           />
-          <Route 
+          {/* <Route 
             path='' 
             element={<Addiction />} 
           />
@@ -38,7 +72,7 @@ function App() {
           <Route 
             path=''
             element={<Userpage />}
-          />
+          /> */}
         </Routes>
       </>
     </Router>
