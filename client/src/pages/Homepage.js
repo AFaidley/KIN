f02f08fda97c39de68caff12ebe0a003812fb48d
AFaidley <alviva.faidley@gmail.com>
2022-11-28 20:20:30 -0700

    Worked on landing and homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,6 +1,6 @@
-import React from "react";
-import chronicDisease from "../Assets/images/chronic.png";
-import grief from "../Assets/images/grief.jpeg";
+import React from 'react';
+import chronicDisease from '../Assets/images/chronic.png';
+import grief from '../Assets/images/grief.jpeg';
 import addiction from '../Assets/images/addiction-pic.jpg';
 import ptsd from '../Assets/images/ptsd.jpeg';
 import physicalDisability from '../Assets/images/physical-disability.jpeg';
@@ -8,45 +8,53 @@ import mentalIllness from '../Assets/images/mental-illness.jpg';
 
 const Homepage = () => {
   return (
-    <div id="homepage-cards">
-      <div className="card" style={{ width: "18rem" }}>
+    <div id='homepage-cards'>
+      <div className='card mt-5' style={{ width: '18rem' }}>
         <img
-          className="card-img-top"
+          className='card-img-top'
           src={chronicDisease}
-          alt="Card image cap"
+          alt='Card image cap'
         />
-        <div className="card-body">
-          <p className="card-text">Chronic Diseases</p>
+        <div className='card-body'>
+          <p className='card-text'>Chronic Diseases</p>
         </div>
       </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src={grief} alt="Card image cap" />
-        <div className="card-body">
-          <p className="card-text">Grief</p>
+      <div className='card mt-5' style={{ width: '18rem' }}>
+        <img className='card-img-top' src={grief} alt='Card image cap' />
+        <div className='card-body'>
+          <p className='card-text'>Grief</p>
         </div>
       </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src={addiction} alt="Card image cap" />
-        <div className="card-body">
-          <p className="card-text">Addiction</p>
+      <div className='card mt-5' style={{ width: '18rem' }}>
+        <img className='card-img-top' src={addiction} alt='Card image cap' />
+        <div className='card-body'>
+          <p className='card-text'>Addiction</p>
         </div>
       </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src={ptsd} alt="Card image cap" />
-        <div className="card-body">
-          <p className="card-text">PTSD</p>
+      <div className='card mt-5' style={{ width: '18rem' }}>
+        <img className='card-img-top' src={ptsd} alt='Card image cap' />
+        <div className='card-body'>
+          <p className='card-text'>PTSD</p>
         </div>
       </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src={physicalDisability} alt="Card image cap" />
-        <div className="card-body">
-          <p className="card-text">Phyiscal Disorders</p>
+      <div className='card mt-5' style={{ width: '18rem' }}>
+        <img
+          className='card-img-top'
+          src={physicalDisability}
+          alt='Card image cap'
+        />
+        <div className='card-body'>
+          <p className='card-text'>Phyiscal Disorders</p>
         </div>
       </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img className="card-img-top" src={mentalIllness} alt="Card image cap" />
-        <div className="card-body">
-          <p className="card-text">Mental Illness</p>
+      <div className='card mt-5 align-center' style={{ width: '18rem' }}>
+        <img
+          className='card-img-top'
+          src={mentalIllness}
+          alt='Card image cap'
+        />
+        <div className='card-body'>
+          <p className='card-text'>Mental Illness</p>
         </div>
       </div>
     </div>
--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React from 'react';
 
 const Landingpage = () => {
-    return (
-        <div>Knowledge In Numbers</div>
-    )
-
-    }
-
-
+  return (
+    <div>
+    <div className='jumbotron jumbotron-fluid'>
+      <div className='container'>
+        <h1 className='display-4'>KIN</h1>
+        <p className='lead'>
+          Knowledge in numbers. Kin is defined as friends and family. In a
+          sentence "Hey, have you checked out KIN? I found this great support
+          group for loss of a loved one on KIN!
+        </p>
+      </div>
+      </div>
+      <footer className='footer text-center text-muted'>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+        Made by Levi, Tori and Alviva
+      </footer>
+    </div>
+  );
+};
 
 export default Landingpage;
