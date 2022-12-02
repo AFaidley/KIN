4c5146a14e453a174c3f9f87d5608bb22708ec00
Levi H <levi.heintzelman@gmail.com>
2022-12-01 18:27:10 -0700

    Add seeds

---
+++ server/seeders/seed.js
@@ -0,0 +1,34 @@
+const db = require('../config/connection');
+const { User, Post, Group } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const postSeeds = require('./postSeeds.json');
+const groupSeeds = require('./groupSeeds.json')
+
+db.once('open', async () => {
+  try {
+    await Post.deleteMany({});
+    await User.deleteMany({});
+    await Group.deleteMany({});
+
+    await Group.create(groupSeeds);
+    await User.create(userSeeds);
+
+    for (let i = 0; i < postSeeds.length; i++) {
+      const { _id, username } = await Post.create(postSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: username },
+        {
+          $addToSet: {
+            posts: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
