e214d314ab9222e7c2f99be2ac1f599cd0cccba4
AFaidley <alviva.faidley@gmail.com>
2022-12-03 11:38:17 -0700

    Added functionality for posts to show on all pages now- couldn't test since seeds would be required

--- client/src/pages/PTSD.js
+++ client/src/pages/PTSD.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Modal } from "react-bootstrap";
 import FormPost from "./Form";
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_POST } from '../utils/queries';
+import { Container, Col, Form, Button, Card, Modal } from 'react-bootstrap';
 
 const NewPost = () => {
   const [titleInput, setTitle] = useState("");
   const [postText, setText] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const location = window.location.href.match("([^\/]+$)")[0];
+  const { loading, error, data } = useQuery(GET_POST, {
+    variables: {groupName: location[0].toUpperCase() + location.substring(1)}
+  });
+
+  if (loading) return "loading..."
 
   const handleInputChange = (e) => {
     const { target } = e;
@@ -39,6 +47,22 @@ const NewPost = () => {
   };
   return (
     <>
+    <Container>
+          {data.allPost.map(({_id, title, postText, username, groupName}) => {
+            return (
+              <Card key={_id} border='dark'>
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text>{postText}</Card.Text>
+                  <Card.Text>{username}</Card.Text>
+                  <Card.Text>{groupName}</Card.Text>
+                  {/* <Card.Text>{comments}</Card.Text> */}
+                </Card.Body>
+              </Card>
+            );
+          })}
+      </Container>
+      
       <Link className="btn-lg btn-secondary text-center" id= "createBtn" onClick={() => setShowModal(true)}>Create post</Link>
       <Modal
         size="lg"
