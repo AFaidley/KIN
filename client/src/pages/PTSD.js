44812b23fa0b40742d3e07aafa31a648249a1458
AFaidley <alviva.faidley@gmail.com>
2022-12-03 13:09:16 -0700

    PTSD working

--- client/src/pages/PTSD.js
+++ client/src/pages/PTSD.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import FormPost from "./Form";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import FormPost from './Form';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_POST } from '../utils/queries';
 import { Container, Col, Form, Button, Card, Modal } from 'react-bootstrap';
 
 const NewPost = () => {
-  const [titleInput, setTitle] = useState("");
-  const [postText, setText] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [titleInput, setTitle] = useState('');
+  const [postText, setText] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [showModal, setShowModal] = useState(false);
-  const location = window.location.href.match("([^\/]+$)")[0];
+  const location = window.location.href.match('([^/]+$)')[0];
   const { loading, error, data } = useQuery(GET_POST, {
-    variables: {groupName: location[0].toUpperCase() + location.substring(1)}
+    variables: { groupName: location.toUpperCase() },
   });
 
-  if (loading) return "loading..."
+  if (loading) return 'loading...';
 
   const handleInputChange = (e) => {
     const { target } = e;
@@ -23,9 +23,9 @@ const NewPost = () => {
     console.log(inputType);
     const inputValue = target.value;
 
-    if (inputType === "title") {
+    if (inputType === 'title') {
       setTitle(inputValue);
-    } else if (inputType === "content") {
+    } else if (inputType === 'content') {
       setText(inputValue);
     }
   };
@@ -34,11 +34,11 @@ const NewPost = () => {
     e.preventDefault();
 
     if (!titleInput) {
-      setErrorMessage("Please enter a title");
+      setErrorMessage('Please enter a title');
       return;
     }
     if (!postText) {
-      setErrorMessage("Please enter post content");
+      setErrorMessage('Please enter post content');
       return;
     }
 
@@ -47,36 +47,43 @@ const NewPost = () => {
   };
   return (
     <>
-    <Container>
-          {data.allPost.map(({_id, title, postText, username, groupName}) => {
-            return (
-              <Card key={_id} border='dark'>
-                <Card.Body>
-                  <Card.Title>{title}</Card.Title>
-                  <Card.Text>{postText}</Card.Text>
-                  <Card.Text>{username}</Card.Text>
-                  <Card.Text>{groupName}</Card.Text>
-                  {/* <Card.Text>{comments}</Card.Text> */}
-                </Card.Body>
-              </Card>
-            );
-          })}
+      <Container>
+        {data.allPost.map(({ _id, title, postText, username, groupName }) => {
+          return (
+            <Card key={_id} border='dark'>
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{postText}</Card.Text>
+                <Card.Text>{username}</Card.Text>
+                <Card.Text>{groupName}</Card.Text>
+                {/* <Card.Text>{comments}</Card.Text> */}
+              </Card.Body>
+            </Card>
+          );
+        })}
       </Container>
 
-      <Link className="btn-lg btn-secondary text-center" id= "createBtn" onClick={() => setShowModal(true)}>Create post</Link>
+      <Link
+        className='btn-lg btn-secondary text-center'
+        id='createBtn'
+        onClick={() => setShowModal(true)}
+      >
+        Create post
+      </Link>
       <Modal
-        size="lg"
+        size='lg'
         show={showModal}
         onHide={() => setShowModal(false)}
-        aria-labelledby="newpost-modal"
-        animation={false}>
-      <Modal.Body>
+        aria-labelledby='newpost-modal'
+        animation={false}
+      >
+        <Modal.Body>
           <FormPost
-          handleModalClose={() => setShowModal(false)} 
-          onClick = {handleFormSubmit}
-          onChange = {handleInputChange}>
-          </FormPost>
-      </Modal.Body>
+            handleModalClose={() => setShowModal(false)}
+            onClick={handleFormSubmit}
+            onChange={handleInputChange}
+          ></FormPost>
+        </Modal.Body>
       </Modal>
     </>
   );
